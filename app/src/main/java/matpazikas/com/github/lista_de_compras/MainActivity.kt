18cbf1eb85efa77028b634377fbf66df2b6a3c42
matpazikas <matheuspazikas@gmail.com>
2024-08-12 22:09:01 -0300

    Commit durante a aula

---
+++ app/src/main/java/matpazikas/com/github/lista_de_compras/MainActivity.kt
@@ -0,0 +1,27 @@
+package matpazikas.com.github.lista_de_compras
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import matpazikas.com.github.lista_de_compras.ui.theme.ListadecomprasTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Lista de Compras"
+
+    }
+}
